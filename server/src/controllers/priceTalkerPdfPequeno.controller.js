13446d93906c8aa8e5f88f90c78dce49d7a46fb4
alicedev94 <alicedev94@gmail.com>
2024-01-29 10:22:28 -0400

    first commit 29012024

---
+++ server/src/controllers/priceTalkerPdfPequeno.controller.js
@@ -0,0 +1,459 @@
+// -- LIBRERÍAS --
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+const pathLogo = require("../img/index");
+const bwipjs = require("bwip-js");
+const moment = require("moment-timezone");
+
+// -- VARIABLES --
+
+// Posición
+let boxPositionX = 70;
+let boxPositionY = 30;
+let priceTalkerDescriptionPositionX = 98;
+let priceTalkerDescriptionPositionY = 98;
+let priceTalkerBrandPositionX = 103;
+let priceTalkerBrandPositionY = 81;
+let priceTalkerPositionPriceX = 98;
+let priceTalkerPositionPriceY = 175;
+let priceTalkerLogoPositionX = 339;
+let priceTalkerLogoPositionY = 84;
+let priceTalkerCodeSapX = 212;
+let priceTalkerCodeSapY = 163;
+let priceTalkerPositionWarrantyX = 98;
+let priceTalkerPositionWarrantyY = 234;
+let priceTalkerBarCodeX = 270;
+let priceTalkerBarCodeY = 180;
+
+// Tamaño
+const boxWith = 363;
+const boxHeight = 264;
+let priceTalkerBarCodeWith = 99;
+let priceTalkerBarCodeHeight = 28;
+let priceTalkerLogoWith = 48;
+let priceTalkerLogoHeight = 43;
+
+// Fuente
+const priceTalkerfontSize = 12;
+const priceTalkerFontSizePrice = 30;
+
+// -- Contenido estático
+const priceTalkerWidthText = 221;
+const priceTalkerFontPath =
+  "c:/Users/d.marcano/Desktop/td-habladores-precio-web/server";
+
+// Controlador de flujo para la generación de habladores
+let contador = 0;
+
+// -- Contenido calculado box
+// Box1
+// Box2
+let boxPositionX2 = boxPositionX + boxWith + 2;
+// Box3
+let boxPositionY3 = boxPositionY + boxHeight + 2;
+// Box4
+
+const smallPriceTalker = async (priceTalkerData) => {
+  const doc = new PDFDocument({ size: "A4", layout: "landscape" });
+
+  // Borra el contenido existente
+  for (let i = 0; i < priceTalkerData.length; i++) {
+    const product = priceTalkerData[i];
+
+    // Script para determinar la cantidad de habladores que se deben renderizar por pagina
+    if (contador > 3) {
+      // Passing size to the addPage function
+      doc.addPage({ size: "A4", layout: "landscape" });
+      contador = 0;
+    }
+    if (contador == 0) {
+      // box 1
+      doc
+        .rect(boxPositionX, boxPositionY, boxWith, boxHeight) // X, Y , ALTO Y ANCHO
+        .dash(5, { space: 1 })
+        .stroke();
+
+      // -- MARCA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerBrand.toLocaleUpperCase(),
+          priceTalkerBrandPositionX,
+          priceTalkerBrandPositionY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- DESCRIPCIÓN --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerdescription.toLocaleUpperCase(),
+          priceTalkerDescriptionPositionX,
+          priceTalkerDescriptionPositionY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- LOGO --
+      doc.image(pathLogo, priceTalkerLogoPositionX, priceTalkerLogoPositionY, {
+        fit: [priceTalkerLogoWith, priceTalkerLogoHeight],
+        align: "center",
+        valign: "center",
+      });
+
+      // -- PRECIO --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerFontSizePrice)
+        .text(
+          `$ ${product.priceTalkerPrice}`,
+          priceTalkerPositionPriceX,
+          priceTalkerPositionPriceY
+        );
+
+      // -- CÓDIGO SAP
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerSapCode.toLocaleUpperCase(),
+          priceTalkerCodeSapX,
+          priceTalkerCodeSapY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- TIEMPO DE GARANTÍA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(10)
+        .text(
+          `Tiempo de Garantía ${product.priceTalkerWarranty} días`,
+          priceTalkerPositionWarrantyX,
+          priceTalkerPositionWarrantyY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+    } else if (contador == 1) {
+      // box 2
+      doc
+        .rect(boxPositionX2, boxPositionY, boxWith, boxHeight) // X, Y , ALTO Y ANCHO
+        .dash(5, { space: 1 })
+        .stroke();
+
+      // -- MARCA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerBrand.toLocaleUpperCase(),
+          priceTalkerBrandPositionX + boxWith,
+          priceTalkerBrandPositionY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- DESCRIPCIÓN --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerdescription.toLocaleUpperCase(),
+          priceTalkerDescriptionPositionX + boxWith,
+          priceTalkerDescriptionPositionY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- LOGO --
+      doc.image(
+        pathLogo,
+        priceTalkerLogoPositionX + boxWith,
+        priceTalkerLogoPositionY,
+        {
+          fit: [priceTalkerLogoWith, priceTalkerLogoHeight],
+          align: "center",
+          valign: "center",
+        }
+      );
+
+      // -- PRECIO --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerFontSizePrice)
+        .text(
+          `$ ${product.priceTalkerPrice}`,
+          priceTalkerPositionPriceX + boxWith,
+          priceTalkerPositionPriceY
+        );
+
+      // -- CÓDIGO SAP
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerSapCode.toLocaleUpperCase(),
+          priceTalkerCodeSapX + boxWith,
+          priceTalkerCodeSapY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- TIEMPO DE GARANTÍA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(10)
+        .text(
+          `Tiempo de Garantía ${product.priceTalkerWarranty} días`,
+          priceTalkerPositionWarrantyX + boxWith,
+          priceTalkerPositionWarrantyY,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+    } else if (contador == 2) {
+      // box 3
+      doc
+        .rect(boxPositionX, boxPositionY3, boxWith, boxHeight) // X, Y , ALTO Y ANCHO
+        .dash(5, { space: 1 })
+        .stroke();
+
+      // -- MARCA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerBrand.toLocaleUpperCase(),
+          priceTalkerBrandPositionX,
+          priceTalkerBrandPositionY + boxHeight,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- DESCRIPCIÓN --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerdescription.toLocaleUpperCase(),
+          priceTalkerDescriptionPositionX,
+          priceTalkerDescriptionPositionY + boxHeight,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- LOGO --
+      doc.image(
+        pathLogo,
+        priceTalkerLogoPositionX,
+        priceTalkerLogoPositionY + boxHeight,
+        {
+          fit: [priceTalkerLogoWith, priceTalkerLogoHeight],
+          align: "center",
+          valign: "center",
+        }
+      );
+
+      // -- PRECIO --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial-bold",
+            "Arial Bold.ttf"
+          )
+        )
+        .fontSize(priceTalkerFontSizePrice)
+        .text(
+          `$ ${product.priceTalkerPrice}`,
+          priceTalkerPositionPriceX,
+          priceTalkerPositionPriceY + boxHeight
+        );
+
+      // -- CÓDIGO SAP
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(priceTalkerfontSize)
+        .text(
+          product.priceTalkerSapCode.toLocaleUpperCase(),
+          priceTalkerCodeSapX,
+          priceTalkerCodeSapY + boxHeight,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+
+      // -- TIEMPO DE GARANTÍA --
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "node_modules",
+            "@canvas-fonts",
+            "arial",
+            "Arial.ttf"
+          )
+        )
+        .fontSize(10)
+        .text(
+          `Tiempo de Garantía ${product.priceTalkerWarranty} días`,
+          priceTalkerPositionWarrantyX,
+          priceTalkerPositionWarrantyY + boxHeight,
+          {
+            width: priceTalkerWidthText,
+            align: "center",
+          }
+        );
+    }
+    contador++;
+  }
+
+  contador = 0;
+  doc.end();
+
+  let horaVenezuela = moment()
+    .tz("America/Caracas")
+    .format()
+    .replace(/:/g, "-")
+    .replace(/-/g, "_");
+  const writeStream = doc.pipe(
+    fs.createWriteStream(
+      `${process.env.USERPROFILE}/Documents/Habladores-Precio-Web/${horaVenezuela}g.pdf`
+    )
+  );
+  return writeStream;
+};
+
+module.exports = {
+  smallPriceTalker,
+};
