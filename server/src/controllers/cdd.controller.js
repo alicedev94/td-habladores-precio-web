61d0af1b88fc895f5e0ded3d5891bc088705f647
alicedev94 <alicedev94@gmail.com>
2024-05-09 16:55:20 -0400

    avances con el layout de cdd, se va todo a la chingada si genero sin reiniciar el backend, flata crear una nueva pagina  por hablador maanana toca layout mediano y pequeno de supermercado

--- server/src/controllers/cdd.controller.js
+++ server/src/controllers/cdd.controller.js
@@ -7,15 +7,27 @@ const priceTalkerFontPath = process.cwd();
 // VARIBLES PARA LA PLANTILLA GRANDE DEL HABLADOR DE CDD
 let n1cm = 37.8;
 let divisor = 0;
+let fontSize = 30;
 
-let campos = ["código sap", "descripción", "grupo artículo", "ubicación"];
+let campos = [
+  "código sap",
+  "descripción",
+  "grupo artículo",
+  "ubicación",
+  "cantidad",
+];
 
 let reCuadroTitulo = {
-  x: n1cm * 1.5,
-  y: n1cm * 5,
-  with: n1cm * 9.5,
+  x: n1cm * 2,
+  y: n1cm * 3,
+  with: n1cm * 8.4,
   height: n1cm * 1.5,
   radio: n1cm / 3.5,
+};
+
+let logo = {
+  with: 125,
+  height: 125,
 };
 // FIN DE VARIBLES PARA LA PLANTILLA GRANDE DEL HABLADOR DE CDD
 
@@ -38,17 +50,11 @@ const geneCdd = async (inicio, fin, datos) => {
   // LOGO CENTRAL DE LA PLANTILLA
   logoName = "LOGO_DAKA_SE_FELIZ.png";
 
-  doc.image(
-    `${dirnameLogo}/${logoName}`,
-    300,
-    -20, // priceTalkerLogoPositionY
-    {
-      // pathLogo
-      fit: [200, 200],
-      align: "center",
-      valign: "center",
-    }
-  );
+  doc.image(`${dirnameLogo}/${logoName}`, n1cm * 9, -10, {
+    fit: [logo.with, logo.height],
+    align: "center",
+    valign: "center",
+  });
 
   for (let index = 0; index < campos.length; index++) {
     // RE-CUADRO DEL TITULO
@@ -71,7 +77,7 @@ const geneCdd = async (inicio, fin, datos) => {
           "SpecifyPersonalCondensedBlack-Eg2g.ttf"
         )
       )
-      .fontSize(40)
+      .fontSize(fontSize)
       .text(
         `${campos[index].toLocaleUpperCase()}:`,
         reCuadroTitulo.x,
@@ -80,19 +86,48 @@ const geneCdd = async (inicio, fin, datos) => {
           align: "left",
         }
       ),
-      // VALOR
+      // RECUADRO VALOR
       doc
         .roundedRect(
-          reCuadroTitulo.x + (n1cm*9.5),
+          reCuadroTitulo.x + n1cm * 8.1,
           reCuadroTitulo.y + divisor,
-          400,
+          n1cm * 10,
           reCuadroTitulo.height,
           reCuadroTitulo.radio
         )
         .stroke();
     // CONTROLA LA DISTASNCIA DE CADA RECUADRO POR VUELTA
-    divisor += n1cm * 2;
+    if (index == 3) {
+      // SI ES EL CAMPO CANTIDAD LLEVA OTRO FORMATO
+      reCuadroTitulo.height += reCuadroTitulo.height * 1.5;
+
+      // SUBTITULO EN EL ULTIMO CAMPO
+      doc
+        .font(
+          path.join(
+            priceTalkerFontPath,
+            "fonts",
+            "SpecifyPersonalCondensedBlack-Eg2g.ttf"
+          )
+        )
+        .fontSize(fontSize)
+        .text(
+          "UNIDADES",
+          reCuadroTitulo.x + n1cm * 14.5,
+          reCuadroTitulo.y + n1cm * 3.7 + divisor,
+          {
+            align: "left",
+          }
+        ),
+        // SEGUIR CON EL CURSO DE LA APP
+        (divisor += n1cm * 2);
+    } else {
+      divisor += n1cm * 2;
+    }
   }
+
+  // REINICIAR VARIABLES DESPUES DE USO
+  reCuadroTitulo.X = n1cm * 2;
 
   // FIN DEL DOCUMENTO
   doc.end();
