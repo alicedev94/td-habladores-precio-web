9d63694ae89ae412b0619266e302591a5c431290
alicedev94 <alicedev94@gmail.com>
2024-05-15 14:13:37 -0400

    guardado de las medidas del hablador mediano en caso se cometer alguna equivocacion en el proceso de refactorizacion

--- server/src/controllers/ultimasExistenciasMediano.js
+++ server/src/controllers/ultimasExistenciasMediano.js
@@ -5,38 +5,27 @@ const path = require("path");
 // GLOBAL PRECIO PARA EL CALCULO DE PRECIO SEGUN CADA HALADOR
 var precio = 0;
 
-// Posición
-let priceTalkerDescriptionPositionX = 0;
-let priceTalkerDescriptionPositionY = 49;  
-let priceTalkerBrandPositionX = 51.5;  
-let priceTalkerBrandPositionY = 40.5;  
-
-let priceTalkerPositionPriceX = 113.385;  
-let priceTalkerPositionPriceY = 113.385 - 9.45;  
-
-// NUEVAS VARIABLES PARA EL LAYOUT DE HABLADORES 
+// NUEVAS VARIABLES PARA EL LAYOUT DE HABLADORES
 /* NOTA: LA LETRA (P) AL FINAL DEL CADA VARIABLE INDICA EN ESPAÑOL 
 LA FRASE POSICIÓN */
-const n1cm = 37.80;
-let precioTachadoP = { x:169.98 , y: 166.29};
-let precioFullP = { X:226.78, Y:226.78 };
+const n1cm = 37.8; // REPRESENTACION APROXIMADA DE 1CM CUADRADO EN EL PDF
 
-// let priceTalkerPositionPriceX = 132.285 - 18.9;  
-// let priceTalkerPositionPriceY = 113.385 - 9.45;  
-
-priceTalkerPositionPriceY + 37.795 + 75.59
+let precioTachadoP = { x: n1cm * 4.49, y: n1cm * 4.39 };
+let precioFullP = { X: n1cm * 5.9, Y: n1cm * 5.9 };
+let codigoSap = { x: n1cm * 8.2, y: n1cm * 3.4 };
+let descripcion = {x: n1cm * 8.4, y: n1cm * 4.1 };
 
 // Fuente
-const priceTalkerfontSize = 18;  
-const priceTalkerFontSizePrice = 50;  
-const priceTalkerFontSizePriceNew = 80;  
+const priceTalkerfontSize = 18;
+const priceTalkerFontSizePrice = 50;
+const priceTalkerFontSizePriceNew = 80;
 
 // -- Contenido estático
-const priceTalkerWidthText = n1cm * 6;  
+const priceTalkerWidthText = n1cm * 6;
 const priceTalkerFontPath = process.cwd();
 
 const habladorUltimasM = async (dataCallback, endCallback, priceTalkerData) => {
-  const doc = new PDFDocument({ size: "A4", layout: "portrait" });  
+  const doc = new PDFDocument({ size: "A4", layout: "portrait" });
 
   doc.on("data", dataCallback);
   doc.on("end", endCallback);
@@ -57,46 +46,28 @@ const habladorUltimasM = async (dataCallback, endCallback, priceTalkerData) => {
     doc
       .font(path.join(priceTalkerFontPath, "fonts", "PermanentMarker.ttf"))
       .fontSize(priceTalkerFontSizePrice)
-      .text(
-        `$${precioTachado}`,
-        precioTachadoP.x, 
-        precioTachadoP.y  
-      );
+      .text(`$${precioTachado}`, precioTachadoP.x, precioTachadoP.y);
     // PRECIO
     doc
       .font(path.join(priceTalkerFontPath, "fonts", "PermanentMarker.ttf"))
-      .fontSize(priceTalkerFontSizePriceNew)  
-      .text(
-        `$${precio}`,
-        precioFullP.X,   
-        precioFullP.Y  
-      );
+      .fontSize(priceTalkerFontSizePriceNew)
+      .text(`$${precio}`, precioFullP.X, precioFullP.Y);
     // CODIGO SAP
     doc
       .font(path.join(priceTalkerFontPath, "fonts", "PermanentMarker.ttf"))
-      .fontSize(priceTalkerfontSize)  
-      .text(
-        `${product.priceTalkerSapCode}`,
-        priceTalkerPositionPriceX + 56.695 + 75.59 + 37.80 + 37.80,  
-        priceTalkerPositionPriceY - 9.45 + n1cm,  
-        {
-          width: priceTalkerWidthText,  
-          align: "center",
-        }
-      );
+      .fontSize(priceTalkerfontSize)
+      .text(`${product.priceTalkerSapCode}`, codigoSap.x, codigoSap.y, {
+        width: priceTalkerWidthText,
+        align: "center",
+      });
     // DESCRIPCION DEL ARTICULO
     doc
       .font(path.join(priceTalkerFontPath, "fonts", "PermanentMarker.ttf"))
-      .fontSize(priceTalkerfontSize)  
-      .text(
-        `${product.priceTalkerdescription}`,
-        priceTalkerPositionPriceX + 56.695 + 75.59 + 37.80 + 37.80,  
-        priceTalkerPositionPriceY + 15.12 + n1cm,  
-        {
-          width: priceTalkerWidthText,  
-          align: "center",
-        }
-      );
+      .fontSize(priceTalkerfontSize)
+      .text(`${product.priceTalkerdescription}`, descripcion.x, descripcion.y, {
+        width: priceTalkerWidthText,
+        align: "center",
+      });
   }
 
   doc.end();
