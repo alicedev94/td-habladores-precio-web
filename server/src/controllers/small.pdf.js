aaa2a005e48309bf59ad41c913c00d1c37717681
alicedev94 <alicedev94@gmail.com>
2024-06-24 17:03:09 -0400

    hablador pequeno solo falta la data

--- server/src/controllers/small.pdf.js
+++ server/src/controllers/small.pdf.js
@@ -16,7 +16,45 @@ const label = (doc, contenido, fontSize, x, y) => {
 };
 
 const input = (doc, ancho, alto, x, y) => {
-  doc.roundedRect(x, y, ancho, alto, 2.5).stroke();
+  doc.roundedRect(x, y, ancho, alto, 4).stroke();
+};
+
+const estructura = (doc, x, y, interlineadoTitulo) => {
+  campos.forEach((titulo, index) => {
+    // Label (doc, contenido, fontSize, x, y)
+    label(doc, titulo, fontSize, x, y + 12.5);
+
+    // Campo Titulo
+    // Input (doc, ancho, alto, x, y)
+    input(doc, 145, 40, 40, y);
+    console.log(y);
+
+    // Campo valor
+    // Input (doc, ancho, alto, x, y)
+    input(doc, 200, 40, 190, y);
+
+    // Imagen (doc, ruta, imagen, ancho, alto, x, y)
+    if (index == 0) {
+      imagen(doc, rutaLogo, nombreLogo, 80, 80, 450, y);
+    };
+
+    // Cantidad
+    if (index == 2) {
+      label(doc, "cantidad:".toLocaleUpperCase(), fontSize, 425, y - 20);
+    }
+
+    if (index == 3) {
+      // Unidades
+      label(doc, "unidades".toLocaleUpperCase(), fontSize, 350+100, y + 20);
+      input(doc, 140, 100, 395, y - 60);
+    }
+
+    y += interlineadoTitulo;
+  });
+
+  // Unidades
+  // input(doc, 40, 40, 190, y - 62);
+  // label(doc, "unidades".toLocaleUpperCase(), fontSize, 350, y - interlineadoTitulo);
 };
 
 // LOGO DIRECCION DE LOGO DINAMICO
@@ -35,50 +73,23 @@ const habladorP2 = async (inicio, fin, datos, cantidad, ubicacion, doc) => {
   doc.on("data", inicio);
   doc.on("end", fin);
 
-  let x = 0;
-  let y = 0;
+  let x = 50;
+  let y = 40;
 
-  let interlineadoTitulo = 50;
+  let interlineadoTitulo = 60;
 
-  // Estructura
-  campos.forEach((titulo, index) => {
-    // Label (doc, contenido, fontSize, x, y)
-    label(doc, titulo, fontSize, 10, y);
-    // console.log(y);
-    // label(doc, hab, fontSize, 150, y);
-
-    // Input (doc, ancho, alto, x, y)
-    input(doc, 200, 15, 5, y);
-
-    // Imagen (doc, ruta, imagen, ancho, alto, x, y)
-    imagen(doc, rutaLogo, nombreLogo, 50, 50, 300, 0);
-
-    // Cantidad
-    if (index == 2) {
-      label(doc, "cantidad:", fontSize, 300, y);
-    }
-
-    y += interlineadoTitulo;
-  });
-
-  // Unidades
-  input(doc, 200, 40, 230, y - interlineadoTitulo - 25);
-  label(
-    doc,
-    "unidades",
-    fontSize,
-    350,
-    y - interlineadoTitulo
-  );
+  // Estructura (doc, x, y, interlineadoTitulo)
+  estructura(doc, x, y, interlineadoTitulo);
 
   let primeraPosicion = {
-    codigoSap: 0,
+    codigoSap: 40 + 10,
     descripcion: 50,
     grupoArticulo: 100,
     ubicacion: 150,
-    cordenadaX: 150
+    cordenadaX: 150 + 60,
   };
 
+  // Iteración del hablador
   let conVueltas = 0;
   datos.map((hablador) => {
     // Primera Posición
@@ -92,33 +103,32 @@ const habladorP2 = async (inicio, fin, datos, cantidad, ubicacion, doc) => {
         primeraPosicion.codigoSap
       );
 
-      console.log("Nombre", hablador);
       // Descripción
-      label(
-        doc,
-        hablador.priceTalkerdescription,
-        fontSize,
-        primeraPosicion.cordenadaX,
-        primeraPosicion.descripcion
-      );
+      // label(
+      //   doc,
+      //   hablador.priceTalkerdescription,
+      //   fontSize,
+      //   primeraPosicion.cordenadaX,
+      //   primeraPosicion.descripcion
+      // );
 
       // Grupo Articulo
-      label(
-        doc,
-        hablador.linea,
-        fontSize,
-        primeraPosicion.cordenadaX,
-        primeraPosicion.grupoArticulo
-      );
+      // label(
+      //   doc,
+      //   hablador.linea,
+      //   fontSize,
+      //   primeraPosicion.cordenadaX,
+      //   primeraPosicion.grupoArticulo
+      // );
 
       // Ubicación
-      label(
-        doc,
-        hablador.galpon,
-        fontSize,
-        primeraPosicion.cordenadaX,
-        primeraPosicion.ubicacion
-      );
+      // label(
+      //   doc,
+      //   hablador.galpon,
+      //   fontSize,
+      //   primeraPosicion.cordenadaX,
+      //   primeraPosicion.ubicacion
+      // );
     }
 
     // // Segunda Posición
